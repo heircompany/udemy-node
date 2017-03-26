2353ed192f85d6981dff3e88bf3b4dbcce2bee3e
Joe Grotto <joegrotto@heir.company>
2017-03-26 16:06:07 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require(`fs`);
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let logs = `${now}: ${req.method} ${req.url}`;
+  console.log(logs);
+  fs.appendFile('server.log', logs + '\n', (err) => {
+    if(err) {
+      console.log(`unable to update logs`);
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance');
+// });
+
+hbs.registerHelper('siteName', () => {
+  return 'some bullshit site'
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    pageTitle: 'Home',
+    welcomeMessage: 'sup nubs'
+    });
+  });
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    pageTitle: 'About'
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/404', (req, res) => {
+//   res.send({
+//     error: 404,
+//     message: 'page not found'
+//   });
+// });
+//
+// app.get('/json', (req, res) => {
+//   res.send({
+//     name: 'joe grotto',
+//     likes: [
+//       'pizza',
+//       'traveling',
+//       'adventures',
+//       'ice cream',
+//       'pups'
+//     ]
+//   });
+// });
+
+app.listen(3000, () => {
+  console.log('Application Launched on port:3000');
+});
